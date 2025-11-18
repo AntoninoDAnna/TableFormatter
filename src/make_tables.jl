db4c28dd12d7831bd2f44d605c4fe2a751469981
Antonino DAnna <antonino.danna@estudiante.uam.es>
2025-11-18 20:00:48 +0100

    bug fix magnitude and magnitude10 cannot accept negative value

--- src/make_tables.jl
+++ src/make_tables.jl
@@ -1,6 +1,6 @@
 using Format
-magnitude(x::Real) = x==0.0 ? 1 : floor(Int64,log10(0.5*x))
-magnitude10(x::Real) = x==0.0 ? 2 : ceil(Int64,log10(x));
+magnitude(x::Real) = x==0.0 ? 1 : floor(Int64,log10(abs(0.5*x)))
+magnitude10(x::Real) = x==0.0 ? 2 : ceil(Int64,log10(abs(x)));
 non_zero(a::AbstractArray) = a[findall(x->x!=0.0,a)]
 
 function nonmissing(x::AbstractArray)
@@ -25,19 +25,19 @@ end
 
 to_row(A::AbstractVector;F::Syntax=LaTeXsyntax) = join(A,F.cs).*F.el
 
-function rotate(M::AbstractMatrix) 
+function rotate(M::AbstractMatrix)
     m = typeof(M)(undef,size(M,2),size(M,1))
     [m[i,j] = M[j,i] for i in axes(m,1), j in axes(m,2)]
     return m
 end
 
-function rotate(M::AbstractVector) 
+function rotate(M::AbstractVector)
     m = Matrix{Any}(undef,1,size(M,1))
     [m[1,j] = M[j] for j in axes(m,2)]
     return m
 end
 
-function format_numbers(M::AbstractVector{Union{Missing,T}} where T; F::Syntax=LaTeXsyntax,k...) 
+function format_numbers(M::AbstractVector{Union{Missing,T}} where T; F::Syntax=LaTeXsyntax,k...)
     filter = findall(x->!ismissing(x),M)
     if length(filter) ==0
         return M
@@ -62,7 +62,7 @@ function format_numbers(V::AbstractVector{Int64},zpad::Bool=false;k...)
     return format.(V,width=M,precision =M, zeropadding = zpad);
 end
 
-function format_numbers(V::T;F::Syntax=LaTeXsyntax,custom_precision::Union{Nothing,Int64}=nothing,zpad::Bool=false,k...) where T <:AbstractFloat  
+function format_numbers(V::T;F::Syntax=LaTeXsyntax,custom_precision::Union{Nothing,Int64}=nothing,zpad::Bool=false,k...) where T <:AbstractFloat
     m = isnothing(custom_precision) ? 8 : custom_precision
     M = magnitude10(abs.(V));
     return string(F.ms,format(V,width=M+m+1,precision = m, zeropadding = zpad),F.ms)
@@ -71,7 +71,6 @@ end
 function format_numbers(A::AbstractVector{T};custom_precision::Union{Nothing,Int64}=nothing,zpad::Bool=false,F::Syntax=LaTeXsyntax,k...) where T <:AbstractFloat
     M= maximum(abs.(A));
     m= minimum(abs.(A));
-    println(m)
     m = isnothing(custom_precision) ? -magnitude(m) : custom_precision;
     if zpad && M>1.0
         w = magnitude10(M)+m+1;
@@ -108,8 +107,6 @@ function format_numbers(x::NTuple{N,T} where {N,T<:Real};error_style::String="b"
         val = format(val,precision = m)
         res = string(F.ms,val,"(",join(err, ")("),")",F.ms)
     end
-    println(val, " ", err, " ", merr, " ",mval, " ", m)
-    println(res)
     return res
 end
 
@@ -145,6 +142,8 @@ function format_numbers(M::AbstractVector{NTuple{N,T}} where {N,T<:Real};F::Synt
     error("TableFormatter: non valid error style.")
 end
 
+
+
 function no_ze(M,custom_precision::Vector,F::Syntax,error_style,zpad)
     m = [format_numbers(M[i,j],custom_precision=custom_precision[j],error_style=error_style) for i in axes(M,1), j in axes(M,2)]
     m = [to_row([r...]) for r in eachrow(m)]
@@ -155,18 +154,18 @@ end
 @doc raw"""
     make_table(M::AbstractMatrix; kwargs...)::Vector{String}
 
-make a table from a matrix `M`. Each entries of `M` will be converted into one entry of the final table. 
+make a table from a matrix `M`. Each entries of `M` will be converted into one entry of the final table.
 Each component of the output vector is a row of the matrix.
 
 ## Supported type
-The matrix `M` is an `AbstractMatrix{Any}`, therefore its entry can be of any type. The following are supported 
+The matrix `M` is an `AbstractMatrix{Any}`, therefore its entry can be of any type. The following are supported
 by the `make_table` function
-  - `T<:Real`: any real number is supported and treated as a pure number without error. 
-     By default, if `Float`,they are printed with 8 digits after the comar. 
+  - `T<:Real`: any real number is supported and treated as a pure number without error.
+     By default, if `Float`,they are printed with 8 digits after the comar.
   - `String`: they remain untouch, apart of appending whitespaces for alignment reasons
-  - `NTuple{N,T} where {N,T<:Real}`: this represent a number with error. The first component is the central value, 
+  - `NTuple{N,T} where {N,T<:Real}`: this represent a number with error. The first component is the central value,
      the remaning components are treated as separate errors
-  - `Vector{T} T<:Real`: Vector are printed as $ [...] $, a comar separate each value. 
+  - `Vector{T} T<:Real`: Vector are printed as $ [...] $, a comar separate each value.
   - `Missing`: Missing values are use to indicate an empty entry
 
 The functions operate on eachcolumn to determine the precision, hence the element of a column need to  be of the same type.
@@ -178,24 +177,24 @@ If a column has multiple types elements, an error is thrown.
     - "b": the error is written in brakets: 3.1415(23)
     - "p": the error is written with a `\pm` sign: 3.1415 ± 0.0023
     - "z": if given, the errors within the same columns are forced to have the same number of digits using zeropadding
-  - `custom_precision::Union{Int64,Nothing,Vector{Tuple{Int64,Int64}}}=nothing`: override the detected precision. 
+  - `custom_precision::Union{Int64,Nothing,Vector{Tuple{Int64,Int64}}}=nothing`: override the detected precision.
     - if `Int64` the precision of all the entries is overridden
-    - if `Vector{Tuple{Int64,Int64}}`: the tuple is interepret as `(c, p)`, such that the precision in column `c` is set to `p`. This allows to set diffenents precision for each column. 
+    - if `Vector{Tuple{Int64,Int64}}`: the tuple is interepret as `(c, p)`, such that the precision in column `c` is set to `p`. This allows to set diffenents precision for each column.
   - `zpad::Bool=false`: it forces the zero padding. while `error_style="z"` force zeropadding only with the errors,`zpad` forces it also for central value
-  
+
 ## Example
-```  
+```
     using TableFormatter
 
     M = ["test" (10.984,0.023) 5.00;
           missing  (10.984,0.003) 0.60;
           "test"  (122.34,0.490) 12.23]
 
-    make_table(M); 
+    make_table(M);
     # "test &  \$ 10.984 \\pm 0.023 \$  &  5.00000000\\\\"
     # "     &  \$ 10.984 \\pm 0.003 \$  &  0.60000000\\\\"
     # "test &  \$ 122.34 \\pm 0.49 \$   & 12.23000000\\\\"
-  
+
 
     make_table(M,zpad=true)
     # "test &  \$ 010.984(023) \$  &  \$ 05.00000000 \$ \\\\"
@@ -208,7 +207,7 @@ If a column has multiple types elements, an error is thrown.
     # " \$ [1, 2] \$  &  \$  10.984(023) \$  &  5.000\\\\"
     # " \$ [2, 3] \$  &  \$  10.984(003) \$  &  0.600\\\\"
     # " \$ [4, 5] \$  &  \$ 122.340(490) \$  & 12.230\\\\"
-```        
+```
 """
 function make_table(M::AbstractMatrix;
                     F::Syntax=LaTeXsyntax,
@@ -242,13 +241,12 @@ function make_table(M::AbstractMatrix;
     else
         @warn raw"""
     Multiple types in row and column, ignoring zero padding in errors.
-    To suppress this warning either remove 'z' from error style, 
+    To suppress this warning either remove 'z' from error style,
     or reorganize your table in such a way that each row or each column (header excluded)
-    contains entry of only one type. 
+    contains entry of only one type.
       """
         return no_ze(M,cs,F,filter(c->!(c != 'z'), error_style),zpad)
     end
-
 
     m = if type =='r'
         reduce(hcat,[format_numbers([M[2:end,c]...],custom_precision=cs[c]) for c in axes(M,2)])
@@ -259,7 +257,7 @@ function make_table(M::AbstractMatrix;
     else
         reduce(vcat,rotate.(format_numbers([M[c,:]...], custom_precision=cs[c]) for c in axes(M,1)))
     end
-    println(m)
+
 
     output = Vector{String}(undef,size(M,1))
     if !isnothing(header)
@@ -276,13 +274,13 @@ function make_table(M::AbstractMatrix;
     return output
 end
 
-#= 
+#=
 """
 make_table(data::AbstractVector...;k...)::Vector{String}
 
-It takes a set of AbstractVector and generate a table. Each vector correspond to a column colum of 
+It takes a set of AbstractVector and generate a table. Each vector correspond to a column colum of
 the table and all data vector must have the same number of data. Remember to use `missing` for empty
-table entries. 
+table entries.
 
 ## Example
 v1 = [1,2,3]
